56826fe7ae2d56dc1d73fcf3971a18f0e58f6a62
Tim Stirrat <tstirrat@google.com>
2020-02-29 18:33:27 -0800

    Use mainline release.sh for init.sh

--- .bin/init.sh
+++ .bin/init.sh
@@ -4,7 +4,6 @@ luarocks install --local busted
 luarocks install --local luacov
 luarocks install --local luacov-reporter-lcov
 
-# TODO: use the mainline release.sh once the Mac fix is merged
-curl -s https://raw.githubusercontent.com/tstirrat/BigWigsMods-packager/ts-macos-grep/release.sh | bash -s -- -d -z
+curl -s https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh | bash -s -- -d -z
 
 cp -r .release/LibClassicSpecs/Lib .
